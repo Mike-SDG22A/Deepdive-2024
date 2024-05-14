702089846ac1a59d288bf86d073b734ecc334dde
MK-v5 <mksomna@st.noorderpoort.nl>
2024-05-14 09:54:24 +0200

    added Checkpoints

---
+++ Deepdive 2024/Assets/Scripts/LapScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapScript : MonoBehaviour
+{
+    public int checkPointCount = 0;
+    private int requiredCheckpoints;
+    public int LapCount = 0;
+
+    void Update()
+    {
+        if(LapCount == 3)
+        {
+            Debug.Log("You Win!");
+        }   
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        {
+            // hier wordt een functie aangeroepen
+            if (other.CompareTag("Checkpoint") && this.CompareTag("Player"))
+            {
+                PlayerCheckpointCounter();
+            }
+        }
+    }
+
+    private void PlayerCheckpointCounter()
+    {
+        checkPointCount++;
+        if (checkPointCount > requiredCheckpoints)
+        {
+            if (checkPointCount == 12)
+            {
+                LapCount++;
+                if (LapCount > 0)
+                {
+                    checkPointCount = 0;
+                }
+            }
+        }
+    }
+}
